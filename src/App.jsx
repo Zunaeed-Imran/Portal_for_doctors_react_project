8ced864ca3404612217ace6067d16553e5068c4b
imran <zunaeede@gmail.com>
2024-08-31 14:24:37 +0600

    main - working on home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 
 
 import './App.css'
+import Home from './components/Home';
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Welcome To Anesthesiologists Specilist
-        </h1>
-      </div>
-
-      <div className="card"></div>
+        <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+function Home() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome To Anesthesiologists Specilist
+      </h1>
+    </div>
+  );
+}
+
+export default Home
