ae5a203adbcdfcba9d0e5be4e4beff8b46b4bebd
imran <zunaeede@gmail.com>
2024-09-02 22:56:39 +0600

    main - make a nav bar - and try some react icon and practice react icon.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 import Footer from './components/Footer';
 
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <Home />
-      
+      <Navbar />
 
       <Footer />
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { FaArrowsToEye } from 'react-icons/fa6';
+
+export default function Navbar() {
+  return (
+    <div>
+      <h2><FaArrowsToEye color='red' size='5rem' /> Navber</h2>
+    </div>
+  );
+}
