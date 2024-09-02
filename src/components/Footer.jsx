34c294b07fd58772d5d8ad69ce335445e3fa5bfd
imran <zunaeede@gmail.com>
2024-09-02 23:03:27 +0600

    main - practicing react icon.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,11 @@
-
-
-
-
+import { FaGlobe } from 'react-icons/fa';
 
 export default function Footer() {
-  
-
   return (
-
-
     <div>
-
       {/* footer section. */}
 
       <footer className="pt-5">
-
         {/* <!-- it is social icon section  --> */}
         <section className="border-2	">
           {/* <!-- it is just text and company logo section  --> */}
@@ -22,10 +13,7 @@ export default function Footer() {
           <div className="container flex justify-center px-5">
             <div>
               <a href="#">
-                <i className="fa-solid fa-globe" style={{color: '#3039e3'}}>
-                  {' '}
-                  ITLogiko
-                </i>
+                <i style={{ color: '#3039e3' }}><FaGlobe /> ITLogiko</i>
               </a>
               <p>Grow Your Online Business And Improve Customer Retention</p>
             </div>
@@ -36,31 +24,31 @@ export default function Footer() {
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-linkedin"
-                    style={{color: '#0079fa'}}
+                    style={{ color: '#0079fa' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-square-twitter"
-                    style={{color: '#3378e8'}}
+                    style={{ color: '#3378e8' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-github"
-                    style={{color: '#212326'}}
+                    style={{ color: '#212326' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-google-plus"
-                    style={{color: '#33e84e'}}
+                    style={{ color: '#33e84e' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-square-instagram"
-                    style={{color:'#e833df'}}
+                    style={{ color: '#e833df' }}
                   ></i>
                 </a>
                 <a href="#">
@@ -69,7 +57,7 @@ export default function Footer() {
                 <a href="#">
                   <i
                     className="fa-2x fa-brands fa-youtube"
-                    style={{color: '#f20707'}}
+                    style={{ color: '#f20707' }}
                   ></i>
                 </a>
               </div>
@@ -86,56 +74,56 @@ export default function Footer() {
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-react"
-                    style={{color: '#0f91f5'}}
+                    style={{ color: '#0f91f5' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-laravel"
-                    style={{color: '#f31212'}}
+                    style={{ color: '#f31212' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-php"
-                    style={{color: '#1262ed'}}
+                    style={{ color: '#1262ed' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-node"
-                    style={{color: '#48e54b'}}
+                    style={{ color: '#48e54b' }}
                   ></i>
                 </a>
                 <br />
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-data-base"
-                    style={{color: '#1262ed'}}
+                    style={{ color: '#1262ed' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-css3-alt"
-                    style={{color: '#1f62ff'}}
+                    style={{ color: '#1f62ff' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-docker"
-                    style={{color: '#3b71ce'}}
+                    style={{ color: '#3b71ce' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-git"
-                    style={{color: '#ce453b'}}
+                    style={{ color: '#ce453b' }}
                   ></i>
                 </a>
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-square-gitlab"
-                    style={{color: '#eb5e00'}}
+                    style={{ color: '#eb5e00' }}
                   ></i>
                 </a>
                 <br />
@@ -148,7 +136,7 @@ export default function Footer() {
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-html5"
-                    style={{color: '#dd3636'}}
+                    style={{ color: '#dd3636' }}
                   ></i>
                 </a>
                 <a href="#">
@@ -157,71 +145,41 @@ export default function Footer() {
                 <a href="#">
                   <i
                     className="fa-3x fa-brands fa-bootstrap"
-                    style={{color: '#0561ff'}}
+                    style={{ color: '#0561ff' }}
                   ></i>
                 </a>
               </div>
 
               <div className="col-4">
                 <h6>SERVICE</h6>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Team Augmentation Services</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>App Development Service</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Web Design and Development</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Website Maintenance</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>E-Commerce Solution</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Domain Services</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Hosting Services</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>MVP Service</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>Offshore Ofice Expansion</p>
                 </a>
-                <a
-                  className="text-decoration-none text-black"
-                  href="#"
-                >
+                <a className="text-decoration-none text-black" href="#">
                   <p>End to end Development</p>
                 </a>
               </div>
@@ -233,7 +191,7 @@ export default function Footer() {
                 <p>
                   <i
                     className="fa-regular fa-envelope"
-                    style={{color: '#74C0FC'}}
+                    style={{ color: '#74C0FC' }}
                   ></i>{' '}
                   itlogiko@gmail.com
                 </p>
@@ -243,7 +201,7 @@ export default function Footer() {
                 <p>
                   <i
                     className="fa-solid fa-location-dot"
-                    style={{color: '#3039e3'}}
+                    style={{ color: '#3039e3' }}
                   ></i>
                   Level-10, Shakh Hasina Software Technology Park, Nazir
                   Sonkorpur Road, Jashore-7400, bangladesh.
@@ -255,7 +213,7 @@ export default function Footer() {
 
         <div
           className="text-center text-white p-3"
-          style={{backgroundcolor: '#2e3238'}}
+          style={{ backgroundcolor: '#2e3238' }}
         >
           Copyright@ITLogiko2024.
         </div>
@@ -263,5 +221,3 @@ export default function Footer() {
     </div>
   );
 }
-
-
