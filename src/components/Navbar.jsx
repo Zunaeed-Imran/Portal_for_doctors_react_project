7c0759fde98bc33a773e9e1da5803dfc3e7075fd
imran <zunaeede@gmail.com>
2024-09-03 16:55:43 +0600

    main - flex all the nav item.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,20 +9,20 @@ export default function Navbar() {
   return (
     <div className="flex justify-around fixed w-full bg-[#9f95c9] p-3 px-96">
       <IconContext.Provider value={{ size: '1.5rem', color: '#47352e' }}>
-        <h2 className="px-6">
+        <h2 className="px-6 flex">
           <FaHome /> <a href="#">Home</a>
         </h2>
 
-        <h2 className="px-6">
+        <h2 className="px-6 flex">
           <FaQuestion />
           <a href="#">Know why</a>
         </h2>
 
-        <h2 className="px-6">
+        <h2 className="px-6 flex">
           <TbLogin2 /> <a href="#">Login</a>
         </h2>
 
-        <h2 className="px-6">
+        <h2 className="px-6 flex">
           <FcAlarmClock /> <a href="#">Appoint patient</a>
         </h2>
       </IconContext.Provider>
