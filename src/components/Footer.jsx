a1a1ea4f69ed53e158226379c54b4ce72ec86d53
imran <zunaeede@gmail.com>
2024-09-03 19:36:31 +0600

    main - practicing grid in the footer section.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,12 @@
 export default function Footer() {
   return (
     <>
-      
+      <div className="grid grid-cols-3	">
+        <div className="min-h-[100px] bg-slate-600"></div>
+        <div className="min-h-[100px] bg-slate-200"></div>
+        <div className="min-h-[100px] bg-slate-100"></div>
+      </div>
 
-
-
-
-
-
-
-
-
-
-      
       <div className="bg-[#aac0e3]">
         <hr />
         <div className="flex justify-center p-3">
