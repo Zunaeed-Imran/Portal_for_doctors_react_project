b06881b2ea052480c56ed12037c4b014cb34c4ea
imran <zunaeede@gmail.com>
2024-09-03 10:29:57 +0600

    main - working on the nav-bar color.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <>
-      <Home />
       <Navbar />
+      <Home />
 
       <Footer />
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import { FaArrowsToEye } from 'react-icons/fa6';
+import { FaHome, FaQuestion } from 'react-icons/fa';
+import { FcAlarmClock } from 'react-icons/fc';
+import { TbLogin2 } from 'react-icons/tb';
+import { IconContext } from 'react-icons';
+
+
 
 export default function Navbar() {
   return (
-    <div>
-      <h2><FaArrowsToEye color='red' size='5rem' /> Navber</h2>
+    <div className="flex bg-[#9f95c9]	">
+      <IconContext.Provider value={{ size: '5rem', color: '#47352e' }}>
+        <h2 className="px-6">
+          <FaHome /> Home
+        </h2>
+        <h2 className="px-6">
+          <FaQuestion /> Know why
+        </h2>
+        <h2 className="px-6">
+          <TbLogin2 /> Login
+        </h2>
+        <h2 className="px-6">
+          <FcAlarmClock /> Appoint patient
+        </h2>
+      </IconContext.Provider>
     </div>
   );
 }
