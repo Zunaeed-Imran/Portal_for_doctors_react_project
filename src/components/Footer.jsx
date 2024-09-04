d25465e22db397dfc58cafce47632b0df36f26ce
imran <zunaeede@gmail.com>
2024-09-04 15:48:35 +0600

    main - add some more color on more logo.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,10 @@ import { FaInstagramSquare } from 'react-icons/fa';
 import { FaFacebookSquare } from 'react-icons/fa';
 import { FaYoutube } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
+import { MdAttachEmail } from 'react-icons/md';
+import { FaPhone } from 'react-icons/fa';
+import { TbHospitalCircleFilled } from 'react-icons/tb';
+
 
 
 
@@ -126,9 +130,16 @@ export default function Footer() {
         <div className="min-h-[100px]">
           <h6 className="text-xl font-semibold">ADDRESS</h6>
 
-          <p>yourmail@gmail.com</p>
-          <p>01777-888899</p>
-          <p>5695+5R6, Nowapara Rd, Jashore.</p>
+          <p className="flex items-center gap-2">
+            <MdAttachEmail color="#1d54de" /> yourmail@gmail.com
+          </p>
+          <p className="flex items-center gap-2">
+            <FaPhone color="#36de2a" /> 01777-888899
+          </p>
+          <p className="flex items-center gap-2">
+            <TbHospitalCircleFilled color="#ff0019" /> 5695+5R6, Nowapara Rd,
+            Jashore.
+          </p>
           <p>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29344.36436409143!2d89.19177384882387!3d23.168537741670416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff1061d2b7ed9d%3A0x453e406a9156cdc4!2sJashore%20Sadar%20General%20Hospital!5e0!3m2!1sen!2sbd!4v1725425667801!5m2!1sen!2sbd"
