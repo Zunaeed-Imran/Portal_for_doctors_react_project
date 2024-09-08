c11a23ea1c2dbc3a7069369161ce2e97556b49c3
imran <zunaeede@gmail.com>
2024-09-08 15:31:01 +0600

    main - coancat teh hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,29 +14,29 @@ function Hero() {
           />
           <div>
             <h1 className="text-5xl font-bold">Welcome To AnestheSure</h1>
-            <p className="py-6">
+            <p className="py-3">
               Why people go to Anesthesiologists specilist?
             </p>
             <button className="btn btn-primary">Get Started</button>
-            <p className="py-6">
+            <p className="py-1">
               <b>Surgery: </b> Anesthesiologists play a crucial role in ensuring
               patients are comfortable and pain-free during surgical procedures.
               They administer anesthesia and monitor the patients vital signs
               throughout the surgery.
             </p>
-            <p className="py-6">
+            <p className="py-1">
               <b>Pain Management:</b> Anesthesiologists are involved in managing
               pain in various medical situations, such as chronic pain
               conditions or postoperative pain. They use different techniques
               and medications to help patients manage pain effectively.
             </p>
-            <p className="py-6">
+            <p className="py-1">
               <b>Labor and Delivery: </b> Anesthesiologists are often involved
               in providing pain relief during childbirth. They may administer
               epidurals or other forms of anesthesia to make the process more
               comfortable for the mothe
             </p>
-            <p className="py-3">
+            <p className="py-1">
               <b>And many more</b>
             </p>
           </div>
