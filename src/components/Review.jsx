403be553b5f0cd9bd8d6a0a8cf5c169cb0ce86cc
imran <zunaeede@gmail.com>
2024-09-09 16:48:05 +0600

    main - fix the marque and flex

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types'; // up to the file
-import Marquee from 'react-fast-marquee';
 
 function Review({ img }) {
 
   return (
     <div className='flex'>
-      <Marquee pauseOnHover={true} gradient={true}>
+
         <div className="card bg-base-100 w-56 shadow-xl p-3">
           <div className="avatar p-3 justify-center">
             <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
@@ -47,7 +46,6 @@ function Review({ img }) {
             />
           </div>
         </div>
-      </Marquee>
     </div>
   );
 }
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,15 +1,18 @@
-import Review from "./Review"
+import Review from './Review';
 import manA from '../assets/a.png';
 import manB from '../assets/b.png';
+import Marquee from 'react-fast-marquee';
 
 function ReviewCard() {
   return (
-    <div>
-      <Review img={ manA } />
-      <Review img={manB} />
-      <Review img={ manB } />
-    </div>
+    <Marquee pauseOnHover={true} gradient={true}>
+      <div className="flex justify-center gap-5 items-center">
+        <Review img={manA} />
+        <Review img={manB} />
+        <Review img={manB} />
+      </div>
+    </Marquee>
   );
 }
 
-export default ReviewCard
+export default ReviewCard;
