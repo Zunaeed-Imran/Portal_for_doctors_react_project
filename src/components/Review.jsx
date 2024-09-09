8c99fefabcddcba5ffe4d86ac4a60f46fd73ebc5
imran <zunaeede@gmail.com>
2024-09-09 10:38:32 +0600

    main - adding review section - add some image content for review section.

---
+++ src/components/Review.jsx
@@ -0,0 +1,15 @@
+
+function Review() {
+
+  return (
+    <div>
+      <div className="avatar">
+        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Review
