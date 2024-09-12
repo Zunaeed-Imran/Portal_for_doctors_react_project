4621be13472cf9add34448099750344e44fe68d1
imran <zunaeede@gmail.com>
2024-09-12 17:32:41 +0600

    main - move the footer component. - in the layouts folder.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import CardCard from './components/CardCard';
 import Category from './components/Category';
-import Footer from './components/Footer';
+import Footer from './components/layout/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import PageTitle from './components/PageTitle';
