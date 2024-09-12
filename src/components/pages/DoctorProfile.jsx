e199a9b728c053f756b5cb28db604dd8968796e7
imran <zunaeede@gmail.com>
2024-09-12 20:11:19 +0600

    main - passing data in the doctor profile components.

--- src/components/pages/DoctorProfile.jsx
+++ src/components/pages/DoctorProfile.jsx
@@ -1,9 +1,14 @@
 import DoctorComponent from "../layout/DoctorComponent"
+import doctor1 from '../../assets/avater/1.png';
 
 function DoctorProfile() {
   return (
     <div>
-      <DoctorComponent />
+      <DoctorComponent
+        img={doctor1}
+        desig={'Dr.Vincent Barron'}
+        title={'Family Medicine / General Practitioner'}
+        desc={'Professional Background: Dr. Jane Doe is a board-certified cardiologist with over 15 years of experience in diagnosing and treating cardiovascular diseases.'} />
     </div>
   )
 }
