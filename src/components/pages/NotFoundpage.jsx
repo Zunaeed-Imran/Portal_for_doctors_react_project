e46dc54f7b9138ba61d79527202d6dc778189c73
imran <zunaeede@gmail.com>
2024-09-15 11:54:12 +0600

    main - add redirect button to the home page.

--- src/components/pages/NotFoundpage.jsx
+++ src/components/pages/NotFoundpage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 function NotFoundpage() {
@@ -18,6 +19,25 @@ function NotFoundpage() {
           />
         </svg>
         <span>This Page is Not Found</span>
+      </div>
+      <div role="alert" className="alert alert-success">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>You can back to home page form here</span>
+        <button className="btn btn-accent">
+          <Link to="/">Home</Link>
+        </button>
       </div>
     </div>
   );
