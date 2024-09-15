8c53b7229f3c923ba7729b7c968fe2fa97a8bba0
imran <zunaeede@gmail.com>
2024-09-15 16:49:03 +0600

    main - add content to the appointment scheduling.

--- src/components/pages/AppointMentScheduling.jsx
+++ src/components/pages/AppointMentScheduling.jsx
@@ -1,14 +1,39 @@
+import Navbar from "../Navbar";
 
 function AppointMentScheduling() {
   return (
-    <div>
-      <form action="">
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-      </form>
+    <div className="bg-slate-300">
+      <Navbar />
+      <div className="pt-20 flex items-center justify-center min-h-screen">
+        <form action="">
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="input input-bordered w-full max-w-xs"
+          />{' '}
+          <br />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="input input-bordered w-full max-w-xs"
+          />{' '}
+          <br />
+          <input
+            type="text"
+            placeholder="Reason for Visit"
+            className="input input-bordered w-full max-w-xs"
+          />{' '}
+          <input
+            type="text"
+            placeholder="Existing Conditions"
+            className="input input-bordered w-full max-w-xs"
+          />{' '}
+          <br />
+          <button className="btn btn-outline btn-success" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
