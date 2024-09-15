51efd4384ca14d65b548f2321c5151f06a2891a7
imran <zunaeede@gmail.com>
2024-09-15 11:29:46 +0600

    main - adding a not found page.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NotFoundpage from './components/pages/NotFoundpage.jsx'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <NotFoundpage />,
   },
 ]);
 
