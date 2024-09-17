9fc4c44ef1c16310e29f0703f3a12b7b21c2a4fc
imran <zunaeede@gmail.com>
2024-09-17 18:05:51 +0600

    main - remove unnecessary content.

--- src/components/pages/PatientDashboard.jsx
+++ src/components/pages/PatientDashboard.jsx
@@ -18,12 +18,7 @@ function PatientDashboard() {
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex flex-col items-center justify-center">
             {/* Page content here */}
-            <label
-              htmlFor="my-drawer-2"
-              className="btn btn-primary drawer-button lg:hidden"
-            >
-              Open drawer
-            </label>
+            
           </div>
           <div className="drawer-side">
             <label
