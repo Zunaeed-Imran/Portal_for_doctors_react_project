1428b23d58923df05aac43fd7051b062fd35401e
imran <zunaeede@gmail.com>
2024-09-17 16:02:50 +0600

    main - add some content on the patient dashboard page.

--- src/components/pages/PatientDashboard.jsx
+++ src/components/pages/PatientDashboard.jsx
@@ -4,10 +4,32 @@ function PatientDashboard() {
   return (
     <div>
       <div>
-        <Navbar/>
+        <Navbar />
+      </div>
+      <div className="pt-20">
+        <div className="bg-gray-300	">
+          <div className="">
+            <a className="btn btn-ghost text-xl">Profile</a>
+          </div>
+          <div className="">
+            <a className="btn btn-ghost text-xl">Book Appointment</a>
+          </div>
+          <div className="">
+            <a className="btn btn-ghost text-xl">Invoice</a>
+          </div>
+          <div className="">
+            <a className="btn btn-ghost text-xl">Complain</a>
+          </div>
+          <div className="">
+            <a className="btn btn-ghost text-xl">Old Appointment</a>
+          </div>
+          <div className="">
+            <a className="btn btn-ghost text-xl">Report</a>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default PatientDashboard
