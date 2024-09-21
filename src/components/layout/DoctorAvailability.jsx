4184f1f8564a0b313942f95295e5ce62f2289646
imran <zunaeede@gmail.com>
2024-09-21 15:00:32 +0600

    main - done anotehr task. - docotr availability indicator.

--- src/components/layout/DoctorAvailability.jsx
+++ src/components/layout/DoctorAvailability.jsx
@@ -1,11 +1,13 @@
+import { MdEventAvailable } from "react-icons/md";
 
 
 function DoctorAvailability() {
   return (
-    <div>
-      i am available.
+    <div className="flex items-center gap-3 bg-yellow-100">
+      <MdEventAvailable size={"2rem"}/>
+      Doctor is Available.
     </div>
-  )
+  );
 }
 
 export default DoctorAvailability
