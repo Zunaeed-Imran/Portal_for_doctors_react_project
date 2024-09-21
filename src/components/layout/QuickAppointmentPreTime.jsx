15e753b01aa4ae929808bfa6b7e069f54a99d9ba
imran <zunaeede@gmail.com>
2024-09-21 16:02:27 +0600

    main - making new page for adding content of appointment.

---
+++ src/components/layout/QuickAppointmentPreTime.jsx
@@ -0,0 +1,12 @@
+// this page is for Quick Appointment Booking with Pre-set Time Slots.
+// 
+
+function QuickAppointmentPreTime() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QuickAppointmentPreTime
