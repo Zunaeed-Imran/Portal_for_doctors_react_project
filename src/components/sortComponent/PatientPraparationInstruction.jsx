7cc55d06a89ae2a3f3ee0a2ce5850383cfa15571
imran <zunaeede@gmail.com>
2024-09-23 14:00:14 +0600

    main - add another page in the project - patient preparation instruction.

---
+++ src/components/sortComponent/PatientPraparationInstruction.jsx
@@ -0,0 +1,11 @@
+
+
+function PatientPraparationInstruction() {
+  return (
+    <div className="pt-20">
+      <p>from this componenets patient can known about what he have to prapare before next meet.</p>
+    </div>
+  )
+}
+
+export default PatientPraparationInstruction
