b43cb22a92e50d6b73b3a6bfe4528e5306f3a2a0
imran <zunaeede@gmail.com>
2024-09-23 10:26:57 +0600

    main - finally done the task patient history snapshot.

--- src/components/layout/PatientHistory.jsx
+++ src/components/layout/PatientHistory.jsx
@@ -1,12 +1,26 @@
-
+import patientHistory from '../../assets/christine-sandu-jwWtZrm67VI-unsplash.jpg';
 
 
 function PatientHistory() {
   return (
-    <div className="pt-20">
-      History of patient.
+    <div className="pt-20 pl-10">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img src={patientHistory} alt="Snapshot" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            <div className="badge badge-secondary">Patient</div>
+          </h2>
+          <p>Patient History Snapshot</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Take in single look</div>
+            <div className="badge badge-outline">Download</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default PatientHistory
