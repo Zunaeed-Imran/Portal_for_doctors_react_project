86c350c41f507790353c3e7b3006d76299db8ba1
imran <zunaeede@gmail.com>
2024-09-23 19:47:18 +0600

    main - done one more atsk - Patient medicin reminder.

--- src/components/sortComponent/PatientMedicinRemind.jsx
+++ src/components/sortComponent/PatientMedicinRemind.jsx
@@ -4,9 +4,52 @@
 function PatientMedicinRemind() {
   return (
     <div className="pt-20">
-      <p>This is patient medicin knowledeg</p>
+      <div>
+        <p className="font-bold text-center">Weekly Medication</p>
+        <div className="text-center">
+          <div className="">
+            <span className="label-text">Dear Imran,</span>
+          </div>
+          <div className="">
+            <span className="label-text">
+              This is your weekly reminder to stay on top of your medication
+              schedule.
+            </span>
+          </div>
+        </div>
+        <div className="text-center">
+          <p className="font-bold">Important:</p>
+          <p>Drink a full glass of water when taking your medication.</p>
+          <p>
+            If you missed any doses this week, please keep a note and let us
+            know during your next follow-up
+          </p>
+        </div>
+        <div className="text-center">
+          <p className="font-bold">
+            Reminder to Take Your Post-Surgery Medication
+          </p>
+          <p>
+            As part of your recovery process, we’d like to remind you to take
+            your prescribed medication:
+            <li>Pain Reliever: [Medication Name], [Dosage], [Frequency]</li>
+            <li>Antibiotic: [Medication Name], [Dosage], [Frequency]</li>
+          </p>
+        </div>
+        <div className="text-center">
+          <p className="font-bold">Instructions:</p>
+          <p>
+            <li>Take the pain reliever with food to avoid an upset stomach.</li>
+            <li>
+              Complete the full course of antibiotics, even if you feel better.
+            </li>
+            If you experience any unusual symptoms (e.g., rash, severe stomach
+            pain, nausea), please contact our office immediately.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default PatientMedicinRemind
