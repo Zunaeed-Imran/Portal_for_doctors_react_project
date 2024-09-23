b7724772cc9cfaecea118a47cc197e8a0988d034
imran <zunaeede@gmail.com>
2024-09-23 13:56:51 +0600

    main - add another page - patient medicin remember.

---
+++ src/components/sortComponent/PatientMedicinRemind.jsx
@@ -0,0 +1,12 @@
+// from this components patient can know or knowledge about medicin.
+
+
+function PatientMedicinRemind() {
+  return (
+    <div className="pt-20">
+      <p>This is patient medicin knowledeg</p>
+    </div>
+  )
+}
+
+export default PatientMedicinRemind
