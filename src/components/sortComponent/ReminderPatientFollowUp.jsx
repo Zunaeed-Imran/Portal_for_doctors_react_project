54d986a96c1b3f31761c183de6c64753dbf4fdcd
imran <zunaeede@gmail.com>
2024-09-23 18:36:55 +0600

    main - making the page - remainder patient follow up - not done yet.

--- src/components/sortComponent/ReminderPatientFollowUp.jsx
+++ src/components/sortComponent/ReminderPatientFollowUp.jsx
@@ -3,9 +3,22 @@
 function ReminderPatientFollowUp() {
   return (
     <div className="pt-20">
-      Follow upd page.
+      <div>
+        <p className="font-bold text-center">Follow-up Reminder System Page.</p>
+        <div className="text-center">
+          <div className="">
+            <span className="label-text">Dear [Patients Imran],</span>
+          </div>
+          <div className="">
+            <span className="label-text">
+              This is a friendly reminder for your upcoming 2nd follow-up visit
+              with Doctor Younus 22/12/2021 at 6:17 PM.
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ReminderPatientFollowUp
