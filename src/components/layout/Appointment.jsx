e64183e6ca67990366b93dff46871b026b44a41f
imran <zunaeede@gmail.com>
2024-09-26 11:55:37 +0600

    main - done task - give space in the form filed.

--- src/components/layout/Appointment.jsx
+++ src/components/layout/Appointment.jsx
@@ -3,30 +3,27 @@ function Appointment() {
   return (
     <div>
       <div className="pt-20 flex items-center justify-center min-h-screen">
-        <form action="">
+        <form action="" className="flex flex-col gap-2">
           <input
             type="text"
             placeholder="Full Name"
             className="input input-bordered w-full max-w-xs"
-          />{' '}
-          <br />
+          />
           <input
             type="text"
             placeholder="Phone Number"
             className="input input-bordered w-full max-w-xs"
-          />{' '}
-          <br />
+          />
           <input
             type="text"
             placeholder="Reason for Visit"
             className="input input-bordered w-full max-w-xs"
-          />{' '}
+          />
           <input
             type="text"
             placeholder="Existing Conditions"
             className="input input-bordered w-full max-w-xs"
-          />{' '}
-          <br />
+          />
           <button className="btn btn-outline btn-success" type="submit">
             Make Appointment
           </button>
