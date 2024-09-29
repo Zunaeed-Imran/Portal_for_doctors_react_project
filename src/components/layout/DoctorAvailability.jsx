3a1166ba2ccc59c4f92beee20869a8e1f1246af5
imran <zunaeede@gmail.com>
2024-09-29 11:15:33 +0600

    main - change the color of the doctor availability.

--- src/components/layout/DoctorAvailability.jsx
+++ src/components/layout/DoctorAvailability.jsx
@@ -3,8 +3,8 @@ import { MdEventAvailable } from "react-icons/md";
 
 function DoctorAvailability() {
   return (
-    <div className="flex items-center gap-3 bg-yellow-100">
-      <MdEventAvailable size={"2rem"}/>
+    <div className="btn hover:bg-sky-600 flex items-center gap-3 bg-yellow-100">
+      <MdEventAvailable size={'2rem'} />
       Doctor is Available.
     </div>
   );
