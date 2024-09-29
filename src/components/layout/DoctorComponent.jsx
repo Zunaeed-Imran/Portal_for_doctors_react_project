8e6f824b84b34053023344068ab8a8f7e486e8de
imran <zunaeede@gmail.com>
2024-09-29 11:46:29 +0600

    main - done another task - doctor card components have zoom effect.

--- src/components/layout/DoctorComponent.jsx
+++ src/components/layout/DoctorComponent.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function DoctorComponent({img, desig, title, desc}) {
   return (
     <div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 w-96 shadow-xl transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-100 hover:bg-teal-200 duration-300">
         <figure className="px-10 pt-10">
           <img src={img} alt="" />
         </figure>
@@ -14,11 +14,9 @@ function DoctorComponent({img, desig, title, desc}) {
           <h2 className="card-side">{title}</h2>
           <p>{desc}</p>
           <div className="card-actions">
-            <Link to='/appointment'>
-            <button className="btn btn-primary">
-                Make Appointment
-            </button>
-           </Link>
+            <Link to="/appointment">
+              <button className="btn btn-primary">Make Appointment</button>
+            </Link>
           </div>
         </div>
       </div>
