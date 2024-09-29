b303273a6be02ea9b2878dc5d4f7eb20f57480e5
imran <zunaeede@gmail.com>
2024-09-29 09:58:42 +0600

    main - match the nav button with background color.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ import { FaBars } from 'react-icons/fa6';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center fixed w-full bg-[#7c89a1] p-3 px-4 md:px-10 lg:px-20 z-50">
+    <div className="flex justify-between items-center fixed w-full bg-teal-400	 p-3 px-4 md:px-10 lg:px-20 z-50">
       <div className="flex items-center">
         <button className="md:hidden">
           {' '}
@@ -26,28 +26,28 @@ export default function Navbar() {
             {/* Hidden on mobile */}
             <Link
               to="/"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-[#7c9dcf] hover:bg-green-500"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-cyan-300	 hover:bg-sky-600"
             >
               <FaHome />
               Home
             </Link>
             <Link
               to="/DoctorP"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-[#7c9dcf] hover:bg-green-500"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-cyan-300	 hover:bg-sky-600"
             >
               <FaQuestion />
               Doctors
             </Link>
             <Link
               to="/appointment"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-[#7c9dcf] hover:bg-green-500"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-cyan-300	 hover:bg-sky-600"
             >
               <FcButtingIn />
               Appointment
             </Link>
             <Link
               to="/patientDash"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-[#7c9dcf] hover:bg-green-500"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-cyan-300	 hover:bg-sky-600"
             >
               <FcBadDecision />
               Patient_Dashboard
