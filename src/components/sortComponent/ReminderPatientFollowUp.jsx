4167e058968a438bce51a4789c00722154096e65
imran <zunaeede@gmail.com>
2024-09-29 16:39:33 +0600

    main - done one mroe task. remainder patient followup page fix the style.

--- src/components/sortComponent/ReminderPatientFollowUp.jsx
+++ src/components/sortComponent/ReminderPatientFollowUp.jsx
@@ -2,52 +2,52 @@
 
 function ReminderPatientFollowUp() {
   return (
-    <div className="pt-20">
+    <div className="ml-10">
       <div>
-        <p className="font-bold text-center">Follow-up Reminder System Page.</p>
-        <div className="text-center">
+        <h1 className="text-3xl font-bold pb-2">Follow-up Reminder System Page.</h1>
+        <div className="">
           <div className="">
-            <span className="label-text">Dear [Patients Imran],</span>
+            <span className="label-text text-xl pb-3">Dear [Patients Imran],</span>
           </div>
-          <div className="">
-            <span className="label-text">
+          <div className="pb-3">
+            <span className="label-text text-xl">
               This is a friendly reminder for your upcoming 2nd follow-up visit
               with Doctor Younus 22/12/2021 at 6:17 PM.
             </span>
           </div>
         </div>
-        <div className="text-center">
-          <p className="font-bold">What to Bring:</p>
-          <p>
+        <div className="pb-2">
+          <p className="font-bold text-2xl pb-2">What to Bring:</p>
+          <p className="text-xl ">
             Please bring any lab results, imaging (X-ray, MRI, CT scan), or
             other reports from tests that were done after your last visit.
           </p>
         </div>
-        <div className="text-center">
-          <p className="font-bold">Medication List</p>
-          <p>
+        <div className="pb-3">
+          <p className="font-bold text-2xl pb-2">Medication List</p>
+          <p className="text-xl">
             Bring an updated list of all medications you are currently taking,
             including dosage and frequency. Include any over-the-counter drugs
             and supplements.
           </p>
         </div>
-        <div className="text-center">
-          <p className="font-bold">Pain or Symptom Journal</p>
-          <p>
+        <div className="pb-2">
+          <p className="font-bold text-2xl pb-2">Pain or Symptom Journal</p>
+          <p className="text-xl pb-3">
             If you’ve been keeping a journal of your symptoms (such as pain
             levels, swelling, etc.), please bring it with you for review.
           </p>
         </div>
-        <div className="text-center">
-          <p className="font-bold">Insurance Information</p>
-          <p>
+        <div className=" pb-2">
+          <p className="font-bold text-2xl pb-2">Insurance Information</p>
+          <p className="text-xl pb-3">
             Bring your updated insurance card, especially if there has been a
             change in your insurance provider since your last visit.
           </p>
         </div>
-        <div className="text-center">
-          <p className="font-bold">Questions or Concerns</p>
-          <p>
+        <div className="">
+          <p className="font-bold text-2xl">Questions or Concerns</p>
+          <p className="text-xl">
             If you have any questions or concerns about your recovery,
             medications, or symptoms, write them down and bring them with you.
           </p>
