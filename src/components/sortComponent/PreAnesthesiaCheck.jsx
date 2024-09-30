e9e8fb419623d5c8d1fd5e1cd54f98f604171bdb
imran <zunaeede@gmail.com>
2024-09-30 09:55:26 +0600

    main - add two google font in my project - also learn how to use google font.

--- src/components/sortComponent/PreAnesthesiaCheck.jsx
+++ src/components/sortComponent/PreAnesthesiaCheck.jsx
@@ -3,12 +3,12 @@
 
 function PreAnesthesiaCheck() {
   return (
-    <div className="ml-10">
+    <div className="">
       <div>
-        <h1 className="font-bold text-3xl m-5">
+        <h1 className="font-bold text-3xl mb-3 font2">
           This is pre Anesthesia Check list
         </h1>
-        <div>
+        <div className="gap-3">
           <form className="font-bold gap-3" action="">
             <div className="">
               <input
@@ -34,125 +34,125 @@ function PreAnesthesiaCheck() {
           </form>
         </div>
 
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Previous anesthesia history e.g., any adverse reactions
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               History of allergies especially to medications, latex, or foods
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Current medications include herbal supplements or over-the-counter
               drugs
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               History of chronic diseases e.g., asthma, diabetes, hypertension,
               heart disease
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Recent illnesses (e.g., cold, cough, fever)
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Any past surgeries or hospitalizations
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Vital signs (heart rate, blood pressure, oxygen saturation, etc.)
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Airway assessment (e.g., Mallampati score to predict difficulty in
               intubation)
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Respiratory and cardiovascular system examination
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Check for any loose teeth or dental issues
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Fasting status (typically no food 6-8 hours before anesthesia)
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Blood tests (e.g., complete blood count, coagulation profile)
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
-          <label className="label cursor-pointer">
+        <div className="form-control">
+          <label className="cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Electrolytes and glucose levels
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">Chest X-ray (if needed)</span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
-        <div className="form-control items-center">
+        <div className="form-control">
           <label className="label cursor-pointer">
+            <input type="checkbox" defaultChecked className="checkbox" />
             <span className="label-text mr-2">
               Confirm the availability of the surgical team and anesthesiologist
             </span>
-            <input type="checkbox" defaultChecked className="checkbox" />
           </label>
         </div>
         <div>
