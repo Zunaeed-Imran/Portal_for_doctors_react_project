fc2d4b3643cb0e51b2dde709b819fa2dbefea20f
imran <zunaeede@gmail.com>
2024-10-01 17:49:02 +0600

    main - done task - adding content to the login page - basic styling the login page.

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -5,9 +5,16 @@ function Login() {
     <div className="pt-20">
       <div className="pl-10">
         <h1 className="text-3xl font-bold">User Login</h1>
+        <form action="" className="flex flex-col">
+          <label htmlFor="">Email</label>
+          <input type="text" name="email" placeholder="Email" />
+          <label htmlFor="">Password</label>
+          <input type="password" name="password" placeholder="Password" />
+          <button className="btn">Login</button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login
