82bc2627c33fd73745814c67b466d1cbb8d34306
imran <zunaeede@gmail.com>
2024-10-01 16:11:41 +0600

    main - done adding the content on the about page.

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -16,15 +16,37 @@ function AboutUs() {
           concise format for both consumers and healthcare professionals.
         </p>
         <h2 className="text-2xl font-bold mb-3">The AnestheSure Database</h2>
-        <p>
+        <p className="text-xl mb-2">
           The Drugs.com Drug Information Database is powered by several
           independent leading medical-information suppliers, including; American
           Society of Health-System Pharmacists, Cerner Multum and Micromedex.
           Individual drug (or drug-class) content compiled by these sources is
           peer reviewed and delivered by Drugs.com.
         </p>
-        <p className="">
+        <p className="text-xl mb-3">
           AnestheSure also publishes health content from other sources such as;
+        </p>
+        <h2 className="text-2xl font-bold mb-3">Harvard Health Publications</h2>
+        <p className="text-xl mb-3">
+          Articles include Harvard Health Topics A to Z and Harvard Health
+          Decision Guides (used in our Symptom Checker). Harvard Health
+          Publications is the publishing division of the Harvard Medical School
+          of Harvard University and draws on the expertise of the 9,000 faculty
+          physicians to provide authoritative and trustworthy consumer health
+          information.
+        </p>
+        <h2 className="text-2xl font-bold mb-3">DVMetrics - Animalytics</h2>
+        <p className="text-xl pb-10">
+          The Drugs.com Veterinary Product Database is an online version of the
+          Compendium of Veterinary Products published by DVMetrics. The
+          Compendium of Veterinary Products is the animal health industrys
+          largest and most comprehensive database of more than 6,000
+          pharmaceutical, biological, parasiticide and supplement product
+          monographs. DVMetrics is the home for some of the most trusted
+          resources used by veterinary professionals in North America including
+          the North American Compendium of Veterinary Products and the Safety
+          Data Sheet Library which has been available for over 30 and 25 years,
+          respectively.
         </p>
       </div>
 
