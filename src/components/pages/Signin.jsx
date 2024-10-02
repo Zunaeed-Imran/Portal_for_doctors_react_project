ff292bce114ed9f4435324542758df1439b91c19
imran <zunaeede@gmail.com>
2024-10-02 11:16:21 +0600

    main - design the sign in page.

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function Signin() {
   return (
-    <div className='pt-20'>
-      <div className="pl-10">
-        <h1 className="text-3xl font-bold">Sing Up with new Account</h1>
-        <form action="" className="flex flex-col">
-          <label htmlFor="">Email</label>
-          <input type="text" name="email" placeholder="Email" />
-          <label htmlFor="">Password</label>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="">Re-Type-Password</label>
-          <input type="password" name="password" placeholder="Password" />
-          <button className="btn">Sign-in</button>
-        </form>
+    <div className="">
+      <div className="pt-20 hero">
+        <div className="min-h-screen">
+          <form action="" className="flex flex-col gap-2">
+            <label htmlFor="" className="text-2xl font-semibold m-5">
+              Sing Up with new Account
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="password"
+              placeholder="Set A Password"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <div>
+              <button className="btn btn-primary">Sign-in</button>
+            </div>
+          </form>
+          <div className="py-5">
+            <p>
+              Already have an account?
+              <Link className="link link-primary" to="/login">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
